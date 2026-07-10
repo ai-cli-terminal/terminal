312ae9c8d128b144b268f1b77c2ca931d4578b7e
deepe <deepestdark@gmail.com>
2026-07-11 07:44:53 +0900

    feat(mobile): AI eval 3-인자 C ABI + JNI 표면
    ai_terminal_mobile_eval_line_ai_json(iOS 대비 C ABI)과
    NativeShellBridge.nativeEvalLineAi(Android JNI). 기존 2-인자 표면은 불변.

--- src/mobile_jni.rs
+++ src/mobile_jni.rs
@@ -33,3 +33,31 @@ pub extern "system" fn Java_dev_aiterminal_android_NativeShellBridge_nativeEvalL
         Err(_) => std::ptr::null_mut(),
     }
 }
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn Java_dev_aiterminal_android_NativeShellBridge_nativeEvalLineAi(
+    mut env: JNIEnv,
+    _this: JObject,
+    input: JString,
+    state_json: JString,
+    ai_config_json: JString,
+) -> jstring {
+    let response = match (
+        env.get_string(&input).map(String::from),
+        env.get_string(&state_json).map(String::from),
+        env.get_string(&ai_config_json).map(String::from),
+    ) {
+        (Ok(input), Ok(state_json), Ok(ai_config_json)) => {
+            mobile::eval_line_ai_json(&input, &state_json, &ai_config_json)
+        }
+        (Err(err), _, _) | (_, Err(err), _) | (_, _, Err(err)) => {
+            mobile::error_result_json(format!("failed to read JNI string: {err}"))
+        }
+    };
+
+    match env.new_string(response) {
+        Ok(value) => value.into_raw(),
+        Err(_) => std::ptr::null_mut(),
+    }
+}
